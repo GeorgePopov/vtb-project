a7a291519bda87750c270fe4f54a3a8a9c46707f
GeorgePopov <poitics88888888@gmail.com>
2026-06-13 01:20:26 +0300

    Initial commit

---
+++ src/main/java/org/example/vtbproject/VtbProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.vtbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtbProjectApplication.class, args);
+    }
+
+}
